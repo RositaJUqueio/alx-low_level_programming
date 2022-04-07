88882bc4b86e63adc57b9883fd03655ba4a394e5
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-07 16:52:03 -0700

    task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,28 @@
+
 #include <stdio.h>
 
+
+
 /**
-* main - function begins
-*
-*  Return: function ends
-*/
+ *
+ * *main - function begins
+ *
+ * *
+ *
+ * * Return: function ends
+ *
+ * */
+
+
 
 int main(void)
+
 {
-	puts("\""Programming is like building a multilingual puzzle");
-	Return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
+
+
