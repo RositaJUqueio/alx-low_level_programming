e5345ccd10f030ab0d5f56634173ef85c9f7805a
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-07 17:12:36 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - function begins
+* main - function begins
 *
-* Return: function ends
+*  Return: function ends
 */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-Return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
