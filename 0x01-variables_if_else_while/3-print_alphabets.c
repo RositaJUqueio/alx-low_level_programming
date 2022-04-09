4e0b91ff1aa2cd4a911c52cad453280140af80b1
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-08 19:56:26 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,53 +1,28 @@
-#include <stdio.h>                                                                        
+#include <stdio.h>       
 
-/**                                                                                       
- *
- * * main - Entry point                                                                      
- *
- * *                                                                                         
- *
- * * Return: Always 0 (Success/correct)                                                      
- *
- * */                                                                                        
-
-int main(void)                                                                            
-
-	                                                                                          
-
+/**                                                                          
+* main - Entry point                                                                      
+*                                                                                          
+* Return: Always 0 (Success/correct)                                                      
+*/                                                                                        
+int main(void)                                                                          
 {                                                                                         
 
 	char lower_case;                                                                          
+	char upper_case;                                                                         
+                                                                                         
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{		
+putchar(lower_case);   
+}       	                                                                                          
 
-	char upper_case;                                                                          
+       for (upper_case = 'A'; upper_case <= 'Z'; upper_case++) 
+{
+putchar(upper_case); 
+}                                                                                      
 
-	                                                                                          
+putchar('\n');
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)                                   
+return (0);
 
-	{                                                                                         
-
-		putchar(lower_case);                                                                      
-
-	}                                                                                         
-
-	                                                                                          
-
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)                                   
-
-	{                                                                                         
-
-		putchar(upper_case);                                                                      
-
-	}                                                                                         
-
-	                                                                                          
-
-	putchar('\n');                                                                            
-
-	                                                                                          
-
-	return (0);                                                                               
-
-	                                                                                          
-
-}  
+}	
