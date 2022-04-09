46d00c8455a1fb35726f6b86b92f9b343f425bdf
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-08 20:41:17 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,35 +1,22 @@
-n - Entry point                                        
-
-** Return: Always 0 (Success/correct)                       
-
-*/                                                                                                        
+/**           
+* main - Entry point 
+*
+*  Return: Always 0 (Success/correct)
+*/                                                                          
+                                                                                                        
 
 int main(void)
-
 {                                                                                                         
-
-	int i;                                                                                                    
-
+int i;                                                                                                    
 	                                                                                                          
-
-	for (i = 0 ; i < 10; i++)                                                                                 
-
-	{                                                                                                         
-
-		printf("%i", i);                                                                                          
-
-	}                                                                                                         
-
+for (i = 0 ; i < 10; i++)                                                                                 
+{                                                                                                         
+printf("%i", i);                                                                                          
+}                                                                                                         
 	                                                                                                          
-
-	printf("\n");                                                                                             
-
-	                                                                                                          
-
-	return (0);                                                                                               
-
-	                                                                                                          
+printf("\n");                                                                                             
+	                                                                                                        
+return (0);                                                                                          
 
 }
-
 
