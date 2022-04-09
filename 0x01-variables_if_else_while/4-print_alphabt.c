60728c81b023773ba2f6e109b08bca65cb7fa302
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-08 20:27:08 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,55 +1,28 @@
 #include <stdio.h>                                                                        
 
 /**                                                                                       
- *
- * * main - Entry point                                                                      
- *
- * *                                                                                         
- *
- * * Return: Always 0 (Success/correct)                                                      
- *
- * */                                                                                        
-
-                                                                                          
-
-int main(void)                                                                            
-
-	                                                                                          
-
+* main - Entry point                                                               
+*
+* Return: Always 0 (Success/correct)                                                      
+*/                                                                                        
+int main(void)                                                                            	                                                                                          
 {                                                                                         
 
-	char alphabet;                                                                            
+char alphabet;                                                                           	                                                                                          
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)                                         
+{                                                                                         
+if (alphabet == 'q')                                                                      
+continue;                                                                                 
 
-	                                                                                          
-
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)                                         
-
-	{                                                                                         
-
-		if (alphabet == 'q')                                                                      
-
-			continue;                                                                                 
+else if (alphabet == 'e')                                                                 
+continue;                                                                                 
 
 		                                                                                          
+putchar(alphabet);                                                                        
+}                                                                                     
 
-		else if (alphabet == 'e')                                                                 
+putchar('\n');                                                                    
 
-			continue;                                                                                 
-
-		                                                                                          
-
-		putchar(alphabet);                                                                        
-
-	}                                                                                         
-
-	                                                                                          
-
-	putchar('\n');                                                                            
-
-	                                                                                          
-
-	return (0);                                                                               
-
-	                                                                                          
+return (0);                                                                        
 
 }         
