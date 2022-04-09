35cdc670d56ec7bff8dd0d626d34af2db313eedf
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-08 19:36:02 -0700

    task 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+n - Entry point                                        
+
+** Return: Always 0 (Success/correct)                       
+
+*/                                                                                                        
+
+int main(void)
+
+{                                                                                                         
+
+	int i;                                                                                                    
+
+	                                                                                                          
+
+	for (i = 0 ; i < 10; i++)                                                                                 
+
+	{                                                                                                         
+
+		printf("%i", i);                                                                                          
+
+	}                                                                                                         
+
+	                                                                                                          
+
+	printf("\n");                                                                                             
+
+	                                                                                                          
+
+	return (0);                                                                                               
+
+	                                                                                                          
+
+}
+
+
