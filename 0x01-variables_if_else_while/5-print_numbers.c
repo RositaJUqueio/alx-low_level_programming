b852a249d11794f15db99a25fb739672650b184c
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-08 20:55:35 -0700

    commit for task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**           
 * main - Entry point 
 *
@@ -6,6 +8,7 @@
                                                                                                         
 
 int main(void)
+
 {                                                                                                         
 int i;                                                                                                    
 	                                                                                                          
