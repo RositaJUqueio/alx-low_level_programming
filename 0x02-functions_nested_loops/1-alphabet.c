303c63b24dc660d0350bb5c628d2931a1ac9b939
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-13 06:49:22 -0700

    script prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(voin)
+{
+char c = 'a';
+
+while(c <= 'z')
+{
+_putchar(c);
+c++;
+
+}
+_putchar('\n');
+
+}
