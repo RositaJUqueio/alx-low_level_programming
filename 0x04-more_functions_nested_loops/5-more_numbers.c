d3514014457df082789acb43d245b9283d820935
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-14 17:34:04 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,6 +4,7 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
+
 void more_numbers(void)
 {
 int i, j;
@@ -17,4 +18,5 @@ _putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
+}
 }
