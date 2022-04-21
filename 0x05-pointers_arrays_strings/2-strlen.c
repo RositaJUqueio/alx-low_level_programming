8eac5555d70e4447229fbd63218d44a585b89770
Rosita J uqueio <uqueiorose@gmail.com>
2022-04-21 09:31:37 -0700

    script writes function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - code creates function returns the length of a string.
+ * s : is the input
+ * Return: 0 means code is sucessful
+ */
+
+int _strlen(char *s)
+{
+
+int string_length = 0;
+
+while (*s ! = 0)
+{
+string_length++;
+s++;
+}
+
+return(length);
+}
