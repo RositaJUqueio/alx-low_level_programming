8089c094bc0462e0a66256e4ea650e857b49ff58
Rosita J uqueio <uqueiorose@gmail.com>
2022-05-09 15:43:00 -0700

    header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,14 +1,13 @@
 #ifndef PI_H
 #define PI_H
 
-
-#define PI 3.14159265359  
-
 /*
  * File: 1-pi.h
  * Author: RJ Uqueio
- * Desrciption: Header file that defines a macro named PI
+ * Descriptiuon: Header file that defines a macro named PI
  * PI as an abbreviation for the token 3.14159265359.
  */
+
+#define PI 3.14159265359
 
 #endif
