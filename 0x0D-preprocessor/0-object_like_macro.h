ae6c1cd472a384878cde66a58a15bff0bb641b65
Rosita J uqueio <uqueiorose@gmail.com>
2022-05-09 16:09:24 -0700

    header file that defines a macro named SIZE as an abbreviation for the token 1024.

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,13 +1,6 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
-/*
- * File name: 0-object_like_macro.h
- * Author: RJ Uqueio 
- * Description: Header file that defines a macro named SIZE
- * SIZE as an abbreviation for the token 1024.
- */
-
 #define SIZE 1024 
 
 #endif
