102bcdb93d14504009e95bb5fb5a2b7f56d48ba3
RositaJUqueio <uqueiorose@gmail.com>
2023-02-17 03:20:47 -0800

    correct directory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include stdio.h
+/*
+ * C program that prints exactly "Programming is like building a multilingual puzzle,
+ *  followed by a new line usinf puts function.
+ *
+ * return 0
+ */
+
+int main(){
+	puts("/"Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
