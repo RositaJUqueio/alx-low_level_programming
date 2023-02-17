07f38447060aaca44f4deecb4fc5ead734cb79dc
RositaJUqueio <uqueiorose@gmail.com>
2023-02-17 10:12:05 -0800

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+/*
+ * file: 101-quote.c
+ * Auth: Rosita J Uqueio
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints  and that piece of art is useful" 
+ * - Dora Korpar, 2015-10-19, followed by a new line,
+ *    to the standard error.
+ *
+ * Return: 1.
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+}
