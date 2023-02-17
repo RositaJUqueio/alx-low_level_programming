afeb7d5528bca0c4921aed034f21e745c0615e78
RositaJUqueio <uqueiorose@gmail.com>
2023-02-17 09:59:33 -0800

    betty styling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * file: 6-size.c
  * Auth: Rosita J Uqueio
  */
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints  size of various types on 
+ * main - Prints  size of various types on
  *  the computer it is compiled and run on.
  *
  * Return: 0.
@@ -15,7 +15,7 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
