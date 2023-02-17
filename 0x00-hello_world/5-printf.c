98b166e9b593a2c5e7f843bae1f390f26e0111c5
RositaJUqueio <uqueiorose@gmail.com>
2023-02-17 03:46:11 -0800

    betty styling

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,13 @@
 
 /*
  * main - prints "with proper grammar, but the outcome is a piece of art,"
+ *                          followed by a new line.
  *
- * Return : 0.
+ * Return: Always 0.
  */
 
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
