b90f72ae4d1928cc83ed7beac765890cddd54b42
RositaJUqueio <uqueiorose@gmail.com>
2023-02-18 13:26:00 -0800

    different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+/* Auth: Rosita J uqueio */
+
+#include <stdio.h>
+
+/**
+ * main - prints all possible different
+ *  combinations of two digits.
+ *  separated by ',' followed by a space.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i < 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i == 8 && j == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
