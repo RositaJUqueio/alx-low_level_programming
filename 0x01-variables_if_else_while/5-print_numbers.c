f0c96d7cdfeb858e37113df4a90477d331c6cb6d
RositaJUqueio <uqueiorose@gmail.com>
2023-02-17 22:43:50 -0800

    single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/* Auth: Rosita J uqueio */
+
+#include <stdio.h>
+
+/**
+ * main - t prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
