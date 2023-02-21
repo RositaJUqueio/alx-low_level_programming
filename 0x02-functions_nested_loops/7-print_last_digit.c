0531d0082bfe6d0df65e7adab4d115b54700a1da
RositaJUqueio <uqueiorose@gmail.com>
2023-02-21 15:12:07 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/* Auth: Rosita J uqueio */
+
+#define "main.h"
+
+/**
+ *  print_last_digit -  prints the last digit of a number.
+ * @j: value to take last digit from
+ *
+ *  Return: lastdigit.
+ */
+
+int print_last_digit(int j)
+{
+#define Lastdigit (j % 10)
+
+	_putchar(lastdigit);
+	return (lastdigit);
+}
