c7977a741a6ea7ba1c797270ed1beb2b3d536580
RositaJUqueio <uqueiorose@gmail.com>
2023-02-21 13:12:20 -0800

    petty betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,7 @@
  * _islower -  checks for lowercase character.
  * @c: character to be checked
  *
- * Returns: 1 if c is lowercase or 0 otherwise.
+ * Return: 1 if c is lowercase or 0 otherwise.
  */
 
 int _islower(int c)
