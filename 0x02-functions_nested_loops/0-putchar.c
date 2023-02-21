b05dd4c654e5e19d1a436a915700154abe2e476d
RositaJUqueio <uqueiorose@gmail.com>
2023-02-21 11:41:34 -0800

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/* Auth: Rosita J uqueio */
+
+#include "main.h"
+
+/**
+ * main -  prints _putchar,
+ *  followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
