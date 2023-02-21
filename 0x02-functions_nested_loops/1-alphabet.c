505b74c094c300615a7e2217108e12019d263548
RositaJUqueio <uqueiorose@gmail.com>
2023-02-21 11:53:41 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/* Auth: Rosita J Uqueio */
+
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase,
+ *  followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+}
