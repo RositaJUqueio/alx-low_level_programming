6ed1fea328d8db1e5483d5bbe19cbbf2bf05ed5d
RositaJUqueio <uqueiorose@gmail.com>
2023-02-22 14:32:27 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/* Auth: Rosita J uqueio */
+
+#include "main.h"
+
+/**
+ * add -adds two integers
+ * @a: first integer to be added.
+ * @b: second integer to be added.
+ *
+ * Return: sum of two integers.
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
