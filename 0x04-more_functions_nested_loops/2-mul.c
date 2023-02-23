2a826557ce0099131617b68b9d55de480209ca15
RositaJUqueio <uqueiorose@gmail.com>
2023-02-23 14:27:54 -0800

    two didgit multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/* Auth: Rosita J uqueio */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first integer to be multiplied.
+ * @b: second integer to be multiplied.
+ *
+ * Return: result of multiplication.
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
