72ae74155e6624db9d774a30ef5f8d33a2587d43
RositaJUqueio <uqueiorose@gmail.com>
2023-02-28 07:05:31 -0800

    points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/* Auth: Rosita J Uqueio */
+
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter
+ *  and updates the value it points to to 98.
+ *  @n: pointer to be updated.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
