e9b6291b3562f3631cc6b19024c79712eeddab10
RositaJUqueio <uqueiorose@gmail.com>
2023-02-28 20:30:25 -0800

    prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+/* Auth: Rosita J Uqueio */
+
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ *  starting with the first character, followed by a new line.
+ * @str: string to be printed.
+ */
+
+void puts2(char *str)
+{
+	int i = 0; /* character position */
+
+	while (str[i] != 0)
+	{
+		if (i % 2 == 0) /* check if the current index i is even */
+			_putchar(str[i]);
+		i++;
+	}
+}
