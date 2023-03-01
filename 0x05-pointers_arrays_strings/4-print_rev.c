ab36c2875e2df3ba6bf16742ab40543fd9aa7184
RositaJUqueio <uqueiorose@gmail.com>
2023-02-28 19:58:52 -0800

    updates

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@ void print_rev(char *s)
 
 	len = 0; /* string length */
 
-	while (s[i++] != 0)
+	while (s[i++] != '\0')
 		len++; /* len now stores the number of chars in s*/
 
 	for (i = len - 1; i >= 0; i--)
