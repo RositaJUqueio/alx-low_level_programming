f7cf279c1f26cf46444c5e02b8dad4756e48269d
RositaJUqueio <uqueiorose@gmail.com>
2023-03-03 04:40:30 -0800

    corrections

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,16 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int len = 0;
-
+	
+	/* Calculate length of the string*/
 	while (s[i++])
 		len++;
+/* Reviersing the string */
 
-	for (i = len; i>= 0; i--)
+	for (i = len - 1; i >= len / 2; i--)
 	{
-
+		char temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
 	}
 }
