7eb2a7fb6609bfb063a53e8bc858c9cac4778ec5
RositaJUqueio <uqueiorose@gmail.com>
2023-03-02 21:44:42 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+/* Auth: Rosita J Uqueio */
+
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: string to be checked
+ *
+ * Return: string length.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; ++len)
+	{
+		/**
+		 * s[len] != '\0' loop continues as long as the
+		 * current character is not the null terminator character
+		*/
+	}
+	return (len);
+}
