b63998cd62406ae999c74591963d645589554abd
RositaJUqueio <uqueiorose@gmail.com>
2023-03-10 15:29:50 -0800

    prints file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * program that prints its name,
- *  followed by a new line.
+ * main -  prints its name, followed by a new line.
  *  you rename the program, it will print the new name,
- *   without having to compile it again.
+ * @argc: number of characters
+ * @argv: array containing names of objects passed
  *
  * Return: 0.
  */
@@ -14,8 +14,9 @@
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
-	printf("%s \n",argv[i]);
+	printf("%s \n", argv[i]);
 
 	return (0);
 }
