414d4a70ca5e646ac78b2613b314dd1735246255
RositaJUqueio <uqueiorose@gmail.com>
2023-03-10 15:39:16 -0800

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+/* Auth: Rosita J uqueio */
+
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ *  followed by a new line.
+ * @argc: number of argumets.
+ * @argv: names of arguments.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d \n", argc);
+
+	return (0);
+}
