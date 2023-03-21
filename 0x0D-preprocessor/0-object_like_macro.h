7ba80a6b0aabb941fe96083aca5963bb0a00f4a5
RositaJUqueio <uqueiorose@gmail.com>
2023-03-20 19:37:12 -0700

    decsription

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * Auth: Rosita J Uqueio
+ * Desc: header defines a macro
+ *       SIZE abbreviation for the token 1024.
+ */
+
 #define SIZE 1024
+
+#endif
