729fe64396277f3085ca501b7bcc31309338c429
RositaJUqueio <uqueiorose@gmail.com>
2023-03-22 14:08:23 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* Auth: Rosita J Uqueio
- * Define a new type struct named dog 
+/* Auth: Rosita J Uqueio*/
+
+/*
+ * Desc: Define a new type struct dog
+ * struct dog - new struct defined
+ *  @name: name of dog.
+ *  @age: dog age.
+ *  @owner: owner name.
  */
 
 struct dog
