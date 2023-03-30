e95ea18586aec66b25ac9964e33636b2d4377068
Rosita J uqueio <uqueiorose@gmai.com>
2023-03-30 11:34:21 -0700

    playing with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+/* Auth: Rosita J Uqueio */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != NULL)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
