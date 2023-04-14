ff9d832a41089b9fa13eb302649ffaf3890ca7cb
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-14 16:21:20 -0700

    fixing error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,11 +23,9 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	else
-	{
-		o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		w = write(o, text_content, len);
-	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
