77cd6438a4388ae6bc8e9f6cdc06c2bb513ccd85
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-26 15:51:11 -0700

    test files

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+/* Auth: Rosita J Uqueio */
+
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you
+ * need to flip to get from one number to another.
+ * @n: 
+ * @m:
+ *
+ * Return: e number of bits.
+ */
