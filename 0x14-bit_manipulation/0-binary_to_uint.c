47c8489901aaf1d61504d50f0ce0b73167c6486c
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-28 14:34:51 -0700

    updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,23 +15,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	/* result will hold the converted binary number*/
 
-	while (*b != '\0')
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
-
-		result = result * 2 + (*b - '0'); /* (*b -'0') converts to int*/
-		/* (result * 2)- left shifts the bits*/
-		b++;
-	}
 	if (b == NULL)
-	{
 		return (0);
-	}
-	else
+
+	while (*b)
 	{
-		return (result);
+		if (*b == '0' || *b == '1')
+		{
+			result <<= 1;
+			result += (*b - '0');
+			b++;
+		}
+		else
+		{
+			return (0);
+		}
 	}
+
+	return (result);
 }
