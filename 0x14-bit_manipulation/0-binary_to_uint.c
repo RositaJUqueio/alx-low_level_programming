71fba8c1ab8e4118af6d059b090f802bbfeddedd
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-28 14:16:59 -0700

    changes to improve code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,24 +14,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	/* result will hold the converted binary number*/
+	unsigned int n = 0, mult = 1;
+	int len;
 
-	while (*b != '\0')
+	if (b == '\0')
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		result = result * 2 + (*b - '0'); /* (*b -'0') converts to int*/
-		/* (result * 2)- left shifts the bits*/
-		b++;
+		n += (b[len] - '0') * mult;
+		mult *= 2;
 	}
-	if (b == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		return (result);
-	}
+
+	return (n);
+}
 }
